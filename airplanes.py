ffb49104b663ca8fada37034cb739109c9fab4d7
Gabalba22 <169160555+Gabalba22@users.noreply.github.com>
2024-05-29 12:52:45 +0000

    Airplanes

---
+++ airplanes.py
@@ -0,0 +1,12 @@
+Airplanes
+
+class Airplane(speed, positionx, positiony, fuel):
+    def __init__(self, speed, positionx, positiony, fuel):
+        self.speed = speed
+        self.positionx = positionx
+        self.positiony = positiony
+        self.fuel = fuel
+    def takeoff():
+
+    def landing():
+
